549811fa470fa6808f838ca6b3698cf60ecdf83e
Joseph <josephhe255@gmail.com>
2025-11-14 15:14:50 +0100

    trying to improve the drag and drop ui

--- src/app/word-processor/file-drop.directive.ts
+++ src/app/word-processor/file-drop.directive.ts
@@ -10,23 +10,27 @@ export class FileDropDirective {
   @Output() fileDropped = new EventEmitter<File>();
 
   @HostBinding('style.background-color') background = '#ffffff';
+  @HostBinding('class.drag-over') isDragOver = false;
 
   @HostListener('dragover', ['$event']) onDragOver(evt: DragEvent) {
     evt.preventDefault();
     evt.stopPropagation();
     this.background = '#e2eefd';
+    this.isDragOver = true;
   }
 
   @HostListener('dragleave', ['$event']) public onDragLeave(evt: DragEvent) {
     evt.preventDefault();
     evt.stopPropagation();
     this.background = '#ffffff';
+    this.isDragOver = false;
   }
 
   @HostListener('drop', ['$event']) public ondrop(evt: DragEvent) {
     evt.preventDefault();
     evt.stopPropagation();
     this.background = '#ffffff';
+    this.isDragOver = false;
     const files = evt.dataTransfer?.files;
     if (files && files.length > 0) {
       this.fileDropped.emit(files[0]);
